10ac88d20ce78abad68c35b8d26bf6b97394d0f6
squaresun <5quar3sun@gmail.com>
2019-03-01 16:51:41 +0800

    add add rest protocol and RunServer in cmd

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,4 +1,4 @@
-package main
+package cmd
 
 import (
 	"context"
@@ -10,14 +10,19 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/squaresun/go-grpc-http-rest-microservice-tutorial/pkg/svc/v1"
 
-	"github.com/squaresun/go-grpc-http-rest-microservice-tutorial/pkg/protocol/grpc"
+	"github.com/amsokol/go-grpc-http-rest-microservice-tutorial/pkg/protocol/grpc"
+	"github.com/amsokol/go-grpc-http-rest-microservice-tutorial/pkg/protocol/rest"
 )
 
 // Config is configuration for Server
 type Config struct {
 	// gRPC server start parameters section
-	// gRPC is TCP port to listen by gRPC server
+	// GRPCPort is TCP port to listen by gRPC server
 	GRPCPort string
+
+	// HTTP/REST gateway start parameters section
+	// HTTPPort is TCP port to listen by HTTP/REST gateway
+	HTTPPort string
 
 	// DB Datastore parameters section
 	// DatastoreDBHost is host of database
@@ -37,6 +42,7 @@ func RunServer() error {
 	// get configuration
 	var cfg Config
 	flag.StringVar(&cfg.GRPCPort, "grpc-port", "", "gRPC port to bind")
+	flag.StringVar(&cfg.HTTPPort, "http-port", "", "HTTP port to bind")
 	flag.StringVar(&cfg.DatastoreDBHost, "db-host", "", "Database host")
 	flag.StringVar(&cfg.DatastoreDBUser, "db-user", "", "Database user")
 	flag.StringVar(&cfg.DatastoreDBPassword, "db-password", "", "Database password")
@@ -45,6 +51,10 @@ func RunServer() error {
 
 	if len(cfg.GRPCPort) == 0 {
 		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
+	}
+
+	if len(cfg.HTTPPort) == 0 {
+		return fmt.Errorf("invalid TCP port for HTTP gateway: '%s'", cfg.HTTPPort)
 	}
 
 	// add MySQL driver specific parameter to parse date/time
@@ -64,6 +74,11 @@ func RunServer() error {
 	defer db.Close()
 
 	v1API := v1.NewToDoServiceServer(db)
+
+	// run HTTP gateway
+	go func() {
+		_ = rest.RunServer(ctx, cfg.GRPCPort, cfg.HTTPPort)
+	}()
 
 	return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
 }
